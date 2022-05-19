7210465945d8cbbef6c680c0479b04289c11178a
Golcea Tudor <gltudor96@gmail.com>
2022-05-19 19:07:02 +0300

    Part - 3 Single blog

--- src/components/Home/Post.js
+++ src/components/Home/Post.js
@@ -1,19 +1,27 @@
 import { Col } from "reactstrap";
+import { Link } from "react-router-dom";
 
 function Post({ post }) {
 	return (
 		<Col xs='12' md='6'>
-			<img
-				src={`https://picsum.photos/seed/${post.id}/600`}
+			<Link
+				to={"/blog/" + post.id}
 				style={{
-					height: "300px",
-					objectFit: "cover",
-					width: "100%",
-					marginTop: "50px",
-				}}
-			/>
-			<h2>{post.title}</h2>
-			<p>{post.body}</p>
+					textDecoration: "none",
+					color: "black",
+				}}>
+				<img
+					src={`https://picsum.photos/seed/${post.id}/600`}
+					style={{
+						height: "300px",
+						objectFit: "cover",
+						width: "100%",
+						marginTop: "50px",
+					}}
+				/>
+				<h2>{post.title}</h2>
+				<p>{post.body}</p>
+			</Link>
 		</Col>
 	);
 }
--- src/views/Blog.js
+++ src/views/Blog.js
@@ -1,9 +1,42 @@
 import { useParams } from "react-router-dom";
+import { Container, Row, Col } from "reactstrap";
+import { useState, useEffect } from "react";
 
 function Blog() {
 	const params = useParams();
+	const [post, setPost] = useState(null);
 
-	return <h1>Pagina Blog - {params.blogId}</h1>;
+	const getBlog = async (blogId) => {
+		const responseData = await fetch(
+			"https://jsonplaceholder.typicode.com/posts/" + blogId
+		);
+		const apiPost = await responseData.json();
+		console.log(apiPost);
+		setPost(apiPost);
+	};
+
+	useEffect(() => {
+		if (params && params.blogId) {
+			getBlog(params.blogId);
+		}
+	}, [params]);
+
+	return (
+		<>
+			<Container>
+				<Row>
+					<img />
+				</Row>
+				{post ? (
+					<Row>
+						<h1>{post.title}</h1>
+					</Row>
+				) : (
+					<div>Loading...</div>
+				)}
+			</Container>
+		</>
+	);
 }
 
 export default Blog;
